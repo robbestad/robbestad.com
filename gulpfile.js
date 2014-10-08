81d50a7d031ed882a2e5596e1dc496c49b857f5c
Sven Anders Robbestad <robbestad@gmail.com>
2014-10-08 13:55:02 +0200

    removed some code

--- gulpfile.js
+++ gulpfile.js
@@ -3,29 +3,20 @@ var gulp = require('gulp'),
     scss = require('gulp-sass'),
     changed = require('gulp-changed'),
     csso = require('gulp-csso'),
-//    less = require('gulp-less'),
     uglify = require('gulp-uglify'),
-//    browserify = require('browserify'),
     autoprefixer = require('gulp-autoprefixer'),
     imagemin = require('gulp-imagemin'),
     del = require('del'),
     plumber = require('gulp-plumber'),
     notify = require("gulp-notify"),
-//    sourcemaps = require("gulp-sourcemaps"),
-//    pngcrush = require('imagemin-pngcrush'),
-//    react = require('gulp-react'),
     browserSync = require('browser-sync'),
-//    watchify = require('watchify'),
-//    reactify = require('reactify'),
-//    source = require('vinyl-source-stream'),
-//    buffer = require('vinyl-buffer'),
     reload = browserSync.reload,
     shell = require('gulp-shell'),
     jshint      = require('gulp-jshint'),
     nodemon = require('gulp-nodemon');
 
 // tasks
-gulp.task('default', ['jsbundle','cssbundle', 'lint', 'watch'], function () {
+gulp.task('default', ['cssbundle', 'lint', 'watch'], function () {
     gulp.watch('js/**/*', ['lint', reload]);
 });
 
@@ -49,15 +40,6 @@ var paths = {
     fonts: ['']
 };
 
-
-// js tasks
-
-gulp.task('jsbundle', function () {
-    return gulp.src(['bower_components/rebound-js/rebound.js',
-        'js/app-ready.js'])
-        .pipe(concat('app.min.js'))
-        .pipe(gulp.dest('assets'));
-});
 
 // css tasks
 gulp.task('scss', function () {
